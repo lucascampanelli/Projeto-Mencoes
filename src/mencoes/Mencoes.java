6e9a525590d985489ac2f2e9b4a7a0fb1f5aca67
Lucas Campanelli <48259776+lucascampanelli@users.noreply.github.com>
2019-10-23 01:26:00 -0300

    Add files via upload

---
+++ src/mencoes/Mencoes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mencoes;
+
+/**
+ *
+ * @author Familia Souza
+ */
+public class Mencoes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       index index = new index();
+       index.setVisible(true);
+    }
+    
+}
